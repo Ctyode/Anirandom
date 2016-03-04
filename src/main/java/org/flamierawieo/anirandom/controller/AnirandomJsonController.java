ca6fd50c9240e3037ebbbaae7cd2547658ce2fc7
Ctyode <irribarrenya@gmail.com>
2016-03-03 21:51:02 -0800

    шрифты шрифты

--- src/main/java/org/flamierawieo/anirandom/controller/AnirandomJsonController.java
+++ src/main/java/org/flamierawieo/anirandom/controller/AnirandomJsonController.java
@@ -12,7 +12,11 @@ public class AnirandomJsonController {
     public Anime handle(@RequestParam(value = "genre", defaultValue = "undefined") String genre,
                         @RequestParam(value = "year", defaultValue = "undefined") String year,
                         @RequestParam(value = "rating", defaultValue = "undefined") String rating) {
-        return new Anime("Cowboy Bebop", "In the year 2071, humanity has colonized several of the planets and moons of the solar system leaving the now uninhabitable surface of planet Earth behind. The Inter Solar System Police attempts to keep peace in the galaxy, aided in part by outlaw bounty hunters, referred to as \"Cowboys\". The ragtag team aboard the spaceship Bebop are two such individuals.", "http://cdn.myanimelist.net/images/anime/4/19644l.jpg", 8.83);
+        if(Math.random() < 0.5) {
+            return new Anime("Cowboy Bebop", "In the year 2071, humanity has colonized several of the planets and moons of the solar system leaving the now uninhabitable surface of planet Earth behind. The Inter Solar System Police attempts to keep peace in the galaxy, aided in part by outlaw bounty hunters, referred to as \"Cowboys\". The ragtag team aboard the spaceship Bebop are two such individuals.", "http://cdn.myanimelist.net/images/anime/4/19644l.jpg", 8.83);
+        } else {
+            return new Anime("Fairy Tail", "A superficial but kind-hearted celestial mage, Lucy Heartfilia, joins a famous wizarding guild in Magnolia Town called Fairy Tail, where the members use their powers to earn rewards in exchange for fulfilling quests. Here she befriends an impetuous but faithful dragon-slayer wizard, Natsu Dragneel, who is scouring the land for his missing father. The two form a bond with fellow mages Erza Scarlet and Gray Fullbuster, and together they tackle daunting challenges, taking on several powerful allies and foes along the way.", "http://cdn.myanimelist.net/images/anime/5/18179l.jpg", 8.30);
+        }
     }
 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,23 +12,28 @@ $(function () {
             $title.text($(this).text());
         });
     });
+    var randomizing = false;
     $(".button").click(function() {
-        var genre = $("#genre").find(".title").text();
-        var year = $("#year").find(".title").text();
-        var rating = $("#rating").find(".title").text();
-        $(".info").removeClass("show");
-        setTimeout(function() {
-            $.getJSON("/anirandom.json", {"genre": (genre == "Genre") ? "undefined" : genre,
-                                          "year": (year == "Year") ? "undefined" : year,
-                                          "rating": (rating == "Rating") ? "undefined" : rating}, function(data) {
-                (function($info) {
-                    $info.addClass("show");
-                    $info.find(".image").css("background-image", "url(\"" + data["image"] + "\")");
-                    $info.find(".title").text(data["title"]);
-                    $info.find(".synopsis").text(data["synopsis"]);
-                    $info.find(".rating").text(data["rating"].toString());
-                })($(".info"));
-            });
-        }, 300);
+        if(!randomizing) {
+            randomizing = true;
+            var genre = $("#genre").find(".title").text();
+            var year = $("#year").find(".title").text();
+            var rating = $("#rating").find(".title").text();
+            $(".info").removeClass("show");
+            setTimeout(function() {
+                $.getJSON("/anirandom.json", {"genre": (genre == "Genre") ? "undefined" : genre,
+                    "year": (year == "Year") ? "undefined" : year,
+                    "rating": (rating == "Rating") ? "undefined" : rating}, function(data) {
+                    (function($info) {
+                        randomizing = false;
+                        $info.addClass("show");
+                        $info.find(".image").css("background-image", "url(\"" + data["image"] + "\")");
+                        $info.find(".title").text(data["title"]);
+                        $info.find(".synopsis").text(data["synopsis"]);
+                        $info.find(".rating").text(data["rating"].toString());
+                    })($(".info"));
+                });
+            }, 300);
+        }
     });
 });
