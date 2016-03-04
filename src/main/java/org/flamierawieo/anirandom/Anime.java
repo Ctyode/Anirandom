4fb1c88d5e6fe12a11b63d950edf11fee2bb978c
Nickta Rawieo <rawieo@yandex.ru>
2016-03-04 20:48:02 +0400

    что-то

--- src/main/java/org/flamierawieo/anirandom/Anime.java
+++ src/main/java/org/flamierawieo/anirandom/Anime.java
@@ -1,17 +1,23 @@
 package org.flamierawieo.anirandom;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Anime {
 
-    private final String title;
-    private final String synopsis;
-    private final String image;
-    private final double score;
+    private String title;
+    private String synopsis;
+    private String image;
+    private Double rating;
+    private List<String> genres;
 
-    public Anime(String title, String synopsis, String image, double score) {
+    public Anime(String title, String synopsis, String image, Double rating, Collection<String> genres) {
         this.title = title;
         this.synopsis = synopsis;
         this.image = image;
-        this.score = score;
+        this.rating = rating;
+        this.genres = new ArrayList<>(genres);
     }
 
     public String getTitle() {
@@ -26,8 +32,33 @@ public class Anime {
         return image;
     }
 
-    public double getScore() {
-        return score;
+    public Double getRating() {
+        return rating;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this == o || !(o == null || getClass() != o.getClass()) && title.equals(((Anime) o).title);
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+
+    public Anime merge(Anime o) {
+        if(equals(o)) {
+            List<String> genres = new ArrayList<>();
+            this.genres.stream().filter(genre -> !genres.contains(genre)).forEach(genres::add);
+            o.getGenres().stream().filter(genre -> !genres.contains(genre)).forEach(genres::add);
+            return new Anime(title, synopsis, image, rating, genres);
+        } else {
+            throw new RuntimeException("Not equals");
+        }
     }
 
 }
--- src/main/java/org/flamierawieo/anirandom/controller/AnirandomJsonController.java
+++ src/main/java/org/flamierawieo/anirandom/controller/AnirandomJsonController.java
@@ -1,18 +1,72 @@
 package org.flamierawieo.anirandom.controller;
 
 import org.flamierawieo.anirandom.Anime;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
 @RestController
 public class AnirandomJsonController {
+
+    private static Set<Anime> animePool;
+
+    static {
+        animePool = new HashSet<>();
+        try {
+            JSONParser parser = new JSONParser();
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("src/main/resources/data.json"));
+            jsonArray.forEach(data -> {
+                JSONObject jsonObject = (JSONObject) data;
+                String title = (String) jsonObject.get("title");
+                String synopsis = (String) jsonObject.get("synopsis");
+                String image = (String) jsonObject.get("image");
+                JSONArray genres = (JSONArray) jsonObject.get("genres");
+                Double rating = (Double) jsonObject.get("rating");
+                animePool.add(new Anime(title, synopsis, image, rating, genres));
+            });
+        } catch (IOException | ParseException e) {
+            Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, "", e);
+        }
+    }
+
+    private static Anime anirandom(Set<Anime> pool) {
+        int size = pool.size();
+        int item = new Random().nextInt(size); // In real life, the Random object should be rather more shared than this
+        int i = 0;
+        for(Anime obj : pool) {
+            if (i == item)
+                return obj;
+            i = i + 1;
+        }
+        return null; // ??
+    }
 
     @RequestMapping("/anirandom.json")
     public Anime handle(@RequestParam(value = "genre", defaultValue = "undefined") String genre,
                         @RequestParam(value = "year", defaultValue = "undefined") String year,
                         @RequestParam(value = "rating", defaultValue = "undefined") String rating) {
-        return new Anime("Cowboy Bebop", "In the year 2071, humanity has colonized several of the planets and moons of the solar system leaving the now uninhabitable surface of planet Earth behind. The Inter Solar System Police attempts to keep peace in the galaxy, aided in part by outlaw bounty hunters, referred to as \"Cowboys\". The ragtag team aboard the spaceship Bebop are two such individuals.", "http://cdn.myanimelist.net/images/anime/4/19644l.jpg", 8.83);
+        Set<Anime> pool = new HashSet<>(animePool);
+        if(!"undefined".equals(genre)) {
+            pool = pool.stream().filter(a -> a.getGenres().contains(genre)).collect(Collectors.toSet());
+        }
+        if(!"undefined".equals(rating)) {
+            Double r = Double.parseDouble(rating);
+            pool = pool.stream().filter(a -> a.getRating() >= r).collect(Collectors.toSet());
+        }
+        return anirandom(pool);
     }
 
 }
