373fb6f7cbce1eb1688faa689a1d8f56904cc31e
Nicktael Rawieo <b.d.demin@gmail.com>
2016-04-08 00:33:56 +0400

    код все равно не тестится можно коммитить что захочешь

--- src/main/java/org/flamierawieo/anirandom/Security.java
+++ src/main/java/org/flamierawieo/anirandom/Security.java
@@ -1,7 +1,19 @@
 package org.flamierawieo.anirandom;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public class Security {
 
-    // Rawieo do something
+    private static final byte[] salt = {(byte) 0xc2, (byte) 0x4c, (byte) 0x98, (byte) 0x56,
+                                        (byte) 0x1d, (byte) 0x24, (byte) 0xea, (byte) 0x3b,
+                                        (byte) 0xf3, (byte) 0x73, (byte) 0x1e, (byte) 0x6e,
+                                        (byte) 0x9d, (byte) 0x22, (byte) 0x15, (byte) 0xf2};
+
+    private byte[] pbkdf2WithHmacSHA1(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1").generateSecret(new PBEKeySpec(password.toCharArray(), salt, 1024, 64)).getEncoded();
+    }
 
 }
