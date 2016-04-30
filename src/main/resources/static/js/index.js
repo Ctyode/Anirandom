c58daa085a022fb30dae2c19b00ce78ffe8c40df
Nicktael Rawieo <b.d.demin@gmail.com>
2016-05-01 02:47:40 +0400

    fancy stuff m8

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,13 +2,13 @@ $(function () {
     (function($auth) {
         $auth.find(".auth-button").click(function() {
             if($auth.attr("section") === undefined) {
-                $auth.attr("section", "sign-in");
+                $auth.attr("section", $(this).attr("data-target"));
             } else {
                 $auth.removeAttr("section");
             }
         });
         $auth.find(".goto-button").click(function() {
-            $auth.attr("section", $(this).attr("target"));
+            $auth.attr("section", $(this).attr("data-target"));
         });
     })($(".auth"));
     $(".drop-down-menu").each(function() {
