d973b209dcbc243586ee147ac138d924b8c39b5f
Nicktael Rawieo <b.d.demin@gmail.com>
2016-05-03 18:33:39 +0400

    говнокод который надо неизбежно переписывать

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,22 @@
 $(function () {
+    var update_marquee = function(hold_time, speed) {
+        var title_width = $(".info .title").width();
+        var span_width = $(".info .title span").width();
+        var slide = span_width - title_width;
+        
+        var $wrap = $(".title .wrap");
+        var transition_duration = slide / speed;
+        $wrap.css("transition", "all " + transition_duration + "s linear 0s");
+        var marquee = function() {
+            $wrap.css("left", -slide + "px");
+            setTimeout(function() {
+              $wrap.css("left", "0px");
+            }, transition_duration * 1000 + hold_time);
+            setTimeout(marquee, transition_duration * 1000 * 2 + hold_time * 2);
+        };
+        setTimeout(marquee, hold_time);
+    };
+
     (function($auth) {
         $auth.find(".auth-button").click(function() {
             if($auth.attr("section") === undefined) {
@@ -57,10 +75,11 @@ $(function () {
                             $(".error").removeClass("show");
                             $info.addClass("show");
                             $info.find(".image").css("background-image", "url(\"" + data["image"] + "\")");
-                            $info.find(".title").text(data["title"]);
+                            $info.find(".title .wrap span").text(data["title"]);
                             $info.find(".synopsis").text(data["synopsis"]);
                             $info.find(".rating").text(data["rating"].toString());
                             $info.attr("data-anime-id", data["_id"]);
+                            update_marquee(2000, 50);
                         };
                     })($(".info"))).fail(function() {
                         randomizing = false;
