203ad5f61558877329faa6d90fbc573c9ff15f11
Ctyode <ctyode@gmail.com>
2016-06-18 02:10:10 +0300

    errors

---
+++ src/main/java/org/flamierawieo/anirandom/controller/Error.java
@@ -0,0 +1,43 @@
+package org.flamierawieo.anirandom.controller;
+
+import com.mitchellbosecke.pebble.error.PebbleException;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+public class Error extends Base implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    public Map<String, Object> getContext(HttpServletRequest request) {
+        Map<String, Object> context = super.getContext(request);
+
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            context.put("http_status_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            context.put("http_status_message", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        } else {
+            HttpStatus status = HttpStatus.valueOf(statusCode);
+            context.put("http_status_code", status.value());
+            context.put("http_status_message", status.getReasonPhrase());
+        }
+        return context;
+    }
+
+    @RequestMapping(value = PATH)
+    public String handle(HttpServletRequest request) throws IOException, PebbleException {
+        return render("error.html", getContext(request));
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
