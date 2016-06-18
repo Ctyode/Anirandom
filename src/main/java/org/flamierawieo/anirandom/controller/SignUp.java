ba14ab5cada14a40b8af6b593643e31e1169fe01
Nicktael Rawieo <b.d.demin@gmail.com>
2016-06-18 18:35:45 +0400

    some more

--- src/main/java/org/flamierawieo/anirandom/controller/SignUp.java
+++ src/main/java/org/flamierawieo/anirandom/controller/SignUp.java
@@ -1,6 +1,5 @@
 package org.flamierawieo.anirandom.controller;
 
-import org.flamierawieo.anirandom.Validation;
 import org.flamierawieo.anirandom.orm.dao.UserDao;
 import org.flamierawieo.anirandom.orm.mapping.User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +14,21 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.flamierawieo.anirandom.Security.*;
 
 @RestController
 public class SignUp extends Base {
+
+    private static final String usernameRegexp = "[_\\w]{4,16}";
+    private static final String emailRegexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}";
+
+    public boolean validate(String username, String password, String passwordConfirmation, String email) {
+        return Pattern.matches(usernameRegexp, username) &&
+                Pattern.matches(emailRegexp, email) &&
+                password.length() > 5 && password.equals(passwordConfirmation);
+    }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public void post(@RequestParam("username") String username,
@@ -29,7 +38,7 @@ public class SignUp extends Base {
                      @RequestParam("back") String back,
                      HttpServletResponse response) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
         UserDao userDao = new UserDao();
-        if(new Validation().registrationData(username, password, passwordConfirmation, email)) {
+        if(validate(username, password, passwordConfirmation, email)) {
             String accessToken = randomAccessToken();
             List<String> accessTokens = new ArrayList<>();
             accessTokens.add(accessToken);
